a053cfdb5a8f5af4c339a35a694027ba550175b6
krzypl <krzypl@amu.edu.pl>
2024-06-12 17:48:19 +0200

    next

--- 03_plots.R
+++ 03_plots.R
@@ -4,11 +4,21 @@ library(ggrepel)
 full_ds_an <- read_csv("data/full_ds_an.csv") %>% 
   pivot_longer(cols = summer_1816_temp_an:winter_1816_17_prec_an,
                names_to = "anomaly", values_to = "value_of_anomaly") %>% 
-  mutate(unit4anomaly = ifelse(anomaly == "summer_1816_temp_an" | anomaly == "winter_1816_17_temp_an", "°C", "%"))
+  mutate(unit4anomaly = ifelse(anomaly == "summer_1816_temp_an" | anomaly == "winter_1816_17_temp_an", "°C", "%"),
+         lake_name = factor(lake_name, levels = sort(unique(lake_name), decreasing = TRUE)))
 
-anomalies_plot <- ggplot(full_ds_an, aes(y = lake_name, x = value_of_anomaly)) +
+anomalies_plot <- ggplot(full_ds_an, aes(y = lake_name, x = value_of_anomaly)) + #tutaj mozna zrobic osobno ploty dla temperatury i opadu, zeby utrzymac te same skale; inaczej jest wrazenie, ze sie rozjezdza 
   geom_col() +
   facet_wrap(.~ anomaly, scales = "free")
+
+ggsave(filename = "figures/anomalies_plot.svg",
+       plot = anomalies_plot,
+       device = "svg")
+ggsave(filename = "figures/anomalies_plot.pdf",
+       plot = anomalies_plot,
+       width = 12.5,
+       height = 10,
+       device = "pdf")
 
 #plot varve links to climate-------
 varve2climate <- tribble(
@@ -70,94 +80,19 @@ varve2climate_plot <- ggplot(varve2climate) +
     shape = "Relationship with \n climate variable"
   )
 
-#plot varve thickness time series -------
+ggsave(filename = "figures/varve2climate_plot.svg",
+       plot = varve2climate_plot,
+       device = "svg")
+ggsave(filename = "figures/varve2climate_plot.pdf",
+       plot = varve2climate_plot,
+       width = 8,
+       height = 10,
+       device = "pdf")
 
-full_ds <- read_csv("data/full_ds.csv") %>% 
-  filter(age_CE > 1765 & age_CE < 1867)
+#Nino3.4 Index Reconstruction------
+nino3_4_index <- read_table("https://www.ncei.noaa.gov/pub/data/paleo/treering/reconstructions/enso-li2013.txt", skip = 125)
 
-varve_thick_plot <- ggplot(full_ds, aes(x = age_CE, y = varve_thick)) +
-  geom_line() +
-  geom_point(size = 0.4) +
-  geom_vline(xintercept = 1816, color = "blue", alpha = 0.8) +
-  facet_wrap(.~ lake_name, scales = "free_y")
-
-varve_thick_plot
-
-dark_lamin_thick_plot <- full_ds %>% 
-  filter(!is.na(dark_lamin_thick)) %>% 
-  ggplot(aes(x = age_CE, y = dark_lamin_thick)) +
-  geom_line() +
-  geom_point(size = 0.4) +
-  geom_vline(xintercept = 1816, color = "blue", alpha = 0.8) +
-  facet_wrap(.~ lake_name, scales = "free_y")
-
-dark_lamin_thick_plot
-
-light_lamin_thick_plot <- full_ds %>% 
-  filter(!is.na(light_lamin_thick)) %>% 
-  ggplot(aes(x = age_CE, y = light_lamin_thick)) +
-  geom_line() +
-  geom_point(size = 0.4) +
-  geom_vline(xintercept = 1816, color = "blue", alpha = 0.8) +
-  facet_wrap(.~ lake_name, scales = "free_y")
-
-light_lamin_thick_plot
-
-#plot peak detection results ---------
-##total vt-----
-#add labels to peaks so that the corresponding year is visible
-pd_df_vt_total_mgcv <- read_csv("data/pd_df_vt_total_mgcv.csv")
-
-pd_df_vt_total_mgcv_plot <- ggplot(pd_df_vt_total_mgcv, aes(x = age_CE, y = varve_thick)) +
-  geom_line() +
-  geom_point(size = 0.4) +
-  geom_point(data = filter(pd_df_vt_total_mgcv, peaks_pos == 1),
-             shape = 8, color = "red") +
-  geom_point(data = filter(pd_df_vt_total_mgcv, peaks_neg == 1), 
-             shape = 8, color = "blue") +
-  geom_vline(xintercept = 1816, color = "blue", alpha = 0.8) +
-  geom_text_repel(data = filter(pd_df_vt_total_mgcv,
-                                (peaks_pos == 1 | peaks_neg == 1) & age_CE >= 1805 & age_CE <= 1825),
-            aes(label = as.character(round(age_CE, 0))),
-             vjust = -0.5) +
-  facet_wrap(.~ lake_name, scales = "free_y")
-
-
-
-##light laminae thickness-------
-pd_df_vt_light_mgcv <- read_csv("data/pd_df_vt_light_mgcv.csv")
-
-pd_df_vt_light_mgcv_plot <- ggplot(pd_df_vt_light_mgcv, aes(x = age_CE, y = light_lamin_thick)) +
-  geom_line() +
-  geom_point(size = 0.4) +
-  geom_point(data = filter(pd_df_vt_light_mgcv, peaks_pos == 1),
-             shape = 8, color = "red") +
-  geom_point(data = filter(pd_df_vt_light_mgcv, peaks_neg == 1), 
-             shape = 8, color = "blue") +
-  geom_vline(xintercept = 1816, color = "blue", alpha = 0.8) +
-  geom_text_repel(data = filter(pd_df_vt_light_mgcv,
-                                (peaks_pos == 1 | peaks_neg == 1) & age_CE >= 1805 & age_CE <= 1825),
-                  aes(label = as.character(round(age_CE, 0))),
-                  vjust = -0.5) +
-  facet_wrap(.~ lake_name, scales = "free_y")
-
-pd_df_vt_light_mgcv + xlim(c(1800, 1840))
-
-##dark laminae thickness-------
-pd_df_vt_dark_mgcv <- read_csv("data/pd_df_vt_dark_mgcv.csv")
-
-pd_df_vt_dark_mgcv_plot <- ggplot(pd_df_vt_dark_mgcv, aes(x = age_CE, y = dark_lamin_thick)) +
-  geom_line() +
-  geom_point(size = 0.4) +
-  geom_point(data = filter(pd_df_vt_dark_mgcv, peaks_pos == 1),
-             shape = 8, color = "red") +
-  geom_point(data = filter(pd_df_vt_dark_mgcv, peaks_neg == 1), 
-             shape = 8, color = "blue") +
-  geom_vline(xintercept = 1816, color = "blue", alpha = 0.8) +
-  geom_text_repel(data = filter(pd_df_vt_dark_mgcv,
-                                (peaks_pos == 1 | peaks_neg == 1) & age_CE >= 1805 & age_CE <= 1825),
-                  aes(label = as.character(round(age_CE, 0))),
-                  vjust = -0.5) +
-  facet_wrap(.~ lake_name, scales = "free_y")
-
-pd_df_vt_dark_mgcv + xlim(c(1800, 1840))
+ggplot(nino3_4_index, aes(x = age_AD, y = sst.anom)) +
+  geom_point()+
+  geom_line()+
+  geom_vline(xintercept = 1816)
